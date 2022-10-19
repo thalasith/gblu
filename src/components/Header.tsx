48bff1bcd068cbd33d62d5cfc8e9d2fd5307ed4a
Boris Tsao <boris.tsao@gmail.com>
2022-10-19 12:46:00 -0700

    adding markdowns to the jupyter notebook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,9 +8,10 @@ export default function Header() {
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/">
-              <a>
+              <a className="flex align-middle">
                 <span className="sr-only">The Gowdie</span>
                 <img className="h-8 w-auto sm:h-10" src="/gowdie.png" alt="" />
+                <p className="text-4xl font-bold text-slate-700">The Gowdie</p>
               </a>
             </Link>
           </div>
