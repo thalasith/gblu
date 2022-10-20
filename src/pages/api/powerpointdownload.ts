b2b89a868e4172cf469cb51673ba342f7dcc4556
Boris Tsao <boris.tsao@gmail.com>
2022-10-20 14:32:01 -0700

    fixing to const

--- src/pages/api/powerpointdownload.ts
+++ src/pages/api/powerpointdownload.ts
@@ -8,14 +8,14 @@ const powerpointdownload = async (
   res: NextApiResponse
 ) => {
   // 1. Create a new Presentation
-  let pres = new pptxgen();
+  const pres = new pptxgen();
 
   // 2. Add a Slide
-  let slide = pres.addSlide();
+  const slide = pres.addSlide();
 
   // 3. Add one or more objects (Tables, Shapes, Images, Text and Media) to the Slide
-  let textboxText = "Hello World from PptxGenJS!";
-  let textboxOpts = { x: 1, y: 1, color: "363636" };
+  const textboxText = "Hello World from PptxGenJS!";
+  const textboxOpts = { x: 1, y: 1, color: "363636" };
   slide.addText(textboxText, textboxOpts);
 
   // 4. Save the Presentation
